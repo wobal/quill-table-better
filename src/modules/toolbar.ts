511f52ea07f6b477a9a6436788e151f770b6dfa0
wobal <46934489+wobal@users.noreply.github.com>
2025-06-06 16:17:05 +0200

    fix option nowarn

--- src/modules/toolbar.ts
+++ src/modules/toolbar.ts
@@ -40,7 +40,7 @@ class TableToolbar extends Toolbar {
       this.handlers[format] == null &&
       this.quill.scroll.query(format) == null
     ) {
-      const tableBetterOptions = (this.options || this.quill.options.modules['table-better']) as Options;
+      const tableBetterOptions = this.quill.options.modules['table-better'] as Options;
       tableBetterOptions.nowarn ? null : console.warn('ignoring attaching to nonexistent format', format, input);
       return;
     }
